6f1310177956776bc3aaa4982ce12efa01ed95b9
Yazeed aljuhani <yazeedmalmarwani@gmail.com>
2019-10-18 10:30:09 +0300

    created the front end

---
+++ js/main.js
@@ -0,0 +1,12 @@
+let data = [
+    {
+        fullname : 'yazeed Aljuhani',
+        URL : "https://yazeed2.github.io/Game/index.html",
+        repo : 'https://github.com/Yazeed2/Game',
+        GameName: "Broken Machine",
+        image: '../Images/1.png'
+    },
+    {
+
+    },
+]
